161cacfea04321ae823f209660cc5d4864a79529
Jo Wagner <jowagner@limago.de>
2026-03-12 14:21:11 +0100

    initial commit

--- DatenBankDemo/src/main/Main.java
+++ DatenBankDemo/src/main/Main.java
@@ -1,8 +1,34 @@
 package main;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println( "Hello World!");
+    private static final String DRIVER = "org.h2.Driver";
+    private static final String CONNECTION_STRING = "jdbc:h2:c:/tmp/db/db;AUTO_SERVER=TRUE";
+    
+    public static void main(String[] args) throws Exception{
+       //Class.forName(DRIVER);
+        try(Connection connection = DriverManager.getConnection(CONNECTION_STRING, "sa","")){
+            try(Statement statement = connection.createStatement()){
+               try(ResultSet rs = statement.executeQuery("SELECT * FROM PUBLIC.tbl_persons")){
+                   ResultSetMetaData metaData = rs.getMetaData();
+                   for (int column = 1, n = metaData.getColumnCount(); column <= n ; column++) {
+                       System.out.printf("%-30s",metaData.getColumnName(column));
+                   }
+                   System.out.println();
+                   while(rs.next()){
+                       for (int column = 1, n = metaData.getColumnCount(); column <= n ; column++) {
+                           System.out.printf("%-30s",rs.getString(column));
+                       }
+                       System.out.println();
+                   }
+               }
+            }
+
+
+        }
+
+
     }
 }
